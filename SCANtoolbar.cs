bf601f222e53cb9580465c377ea5b29d32354b88
technogeeky <technogeeky@gmail.com>
2014-05-02 23:49:36 -0400

    removed toolbar dependency (toolbarwrapper handles this)

--- SCANtoolbar.cs
+++ SCANtoolbar.cs
@@ -12,7 +12,6 @@
  */
 
 using UnityEngine;
-using Toolbar;
 
 namespace SCANsat
 {
--- SCANui.cs
+++ SCANui.cs
@@ -14,7 +14,6 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
-using Toolbar;
 
 namespace SCANsat
 {
