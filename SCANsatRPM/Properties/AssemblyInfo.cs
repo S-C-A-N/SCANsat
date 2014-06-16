fae0cafc6dfe4f5abe03abb2e80a72bf42e75edc
DMagic <david.grandy@gmail.com>
2014-06-15 23:44:02 -0400

    Add informational version number to all assemblies

--- SCANsatRPM/Properties/AssemblyInfo.cs
+++ SCANsatRPM/Properties/AssemblyInfo.cs
@@ -6,3 +6,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("SCANsatRPM")]
 [assembly: AssemblyDescription ("RasterPropMonitor / SCANsat interface plugin for Kerbal Space Program")]
 
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.7.24")]
+[assembly: AssemblyInformationalVersion("1.0.7.24")]
+
--- SCANsatkethane/Properties/AssemblyInfo.cs
+++ SCANsatkethane/Properties/AssemblyInfo.cs
@@ -1,26 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SCANkethane")]
+[assembly: AssemblyTitle("SCANsatKethane")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SCANkethane")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("SCANsatKethane")]
+[assembly: AssemblyCopyright("SCANsat Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("49a6f0d6-3821-42e6-8841-06b5ce284563")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.7.24")]
+[assembly: AssemblyInformationalVersion("1.0.7.24")]
