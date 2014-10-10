48be6ebbae3fad92b35655396c77e99780adb056
DMagic <david.grandy@gmail.com>
2014-10-10 17:25:14 -0400

    Start adding buttons to KSC map

--- SCANsat/SCAN_UI/SCANkscMap.cs
+++ SCANsat/SCAN_UI/SCANkscMap.cs
@@ -15,8 +15,9 @@ namespace SCANsat.SCAN_UI
 		private CelestialBody b;
 		private SCANdata data;
 		//private double startUT;
-		private bool overlay_static_dirty; //, notMappingToday, bigmap_dragging;
+		private bool overlay_static_dirty, drop_down_open, projection_drop_down, mapType_drop_down, resources_drop_down, planetoid_drop_down; //, notMappingToday, bigmap_dragging;
 		private Texture2D overlay_static;
+		private Rect ddRect;
 		//private Rect rc = new Rect(0, 0, 0, 0);
 		//private Rect pos_spotmap = new Rect(10f, 10f, 10f, 10f);
 		//private Rect pos_spotmap_x = new Rect(10f, 10f, 25f, 25f);
@@ -35,6 +36,7 @@ namespace SCANsat.SCAN_UI
 
 		internal override void Start()
 		{
+			//Initialize the map object
 			b = Planetarium.fetch.Home;
 			if (bigmap == null)
 			{
@@ -61,12 +63,142 @@ namespace SCANsat.SCAN_UI
 		protected override void DrawWindowPre(int id)
 		{
 			data = SCANUtil.getData(b);
+
+			//Disable any errant drop down menus
+			if (!drop_down_open)
+			{
+				projection_drop_down = false;
+				mapType_drop_down = false;
+				resources_drop_down = false;
+				planetoid_drop_down = false;
+			}
 		}
 
+		//The primary GUI method
 		protected override void DrawWindow(int id)
 		{
 			growS();
 
+			topMenu(id);
+
+			growE();
+
+			toggleBar(id);
+
+			mapDraw(id);
+
+			stopE();
+
+			legendBar(id);
+
+			stopS();
+
+			if (drop_down_open)
+				dropDown(id);
+		}
+
+		protected override void DrawWindowPost(int id)
+		{
+			//Close the drop down menu if the window is clicked anywhere else
+			if (drop_down_open && Event.current.type == EventType.mouseDown && !ddRect.Contains(Event.current.mousePosition))
+				drop_down_open = false;
+		}
+
+		//Draw the drop down buttons along the top of the map - Used to control map types
+		private void topMenu (int id)
+		{
+			growE();
+
+			GUILayout.Space(50);
+
+			if (GUILayout.Button("Projection", SCANskins.SCAN_buttonFixed, GUILayout.MaxWidth(60)))
+			{
+				projection_drop_down = !projection_drop_down;
+				drop_down_open = !drop_down_open;
+			}
+
+			GUILayout.Space(30);
+
+			if (GUILayout.Button("Map Type", SCANskins.SCAN_buttonFixed, GUILayout.MaxWidth(50)))
+			{
+				mapType_drop_down = !mapType_drop_down;
+				drop_down_open = !drop_down_open;
+			}
+
+			GUILayout.FlexibleSpace();
+
+			if (GUILayout.Button("Resources", SCANskins.SCAN_buttonFixed, GUILayout.MaxWidth(60)))
+			{
+				resources_drop_down = !resources_drop_down;
+				drop_down_open = !drop_down_open;
+			}
+
+			GUILayout.Space(30);
+
+			if (GUILayout.Button("Planetoid", SCANskins.SCAN_buttonFixed, GUILayout.MaxWidth(50)))
+			{
+				planetoid_drop_down = !planetoid_drop_down;
+				drop_down_open = !drop_down_open;
+			}
+
+			GUILayout.Space(50);
+
+			stopE();
+		}
+
+		//Draw the overlay options along the left side of the map texture
+		private void toggleBar (int id)
+		{
+			growS();
+
+			if (GUILayout.Button("Color", SCANskins.SCAN_buttonFixed))
+			{
+				if (SCANcontroller.controller.colours == 0)
+					SCANcontroller.controller.colours = 1;
+				else
+					SCANcontroller.controller.colours = 0;
+				data.resetImages();
+				bigmap.resetMap();
+			}
+
+			if (GUILayout.Button("Grid", SCANskins.SCAN_buttonFixed))
+			{
+				SCANcontroller.controller.map_grid = !SCANcontroller.controller.map_grid;
+				overlay_static_dirty = true;
+			}
+
+			if (GUILayout.Button("Markers", SCANskins.SCAN_buttonFixed))
+			{
+				SCANcontroller.controller.map_markers = !SCANcontroller.controller.map_markers;
+			}
+
+			if (GUILayout.Button("Flags", SCANskins.SCAN_buttonFixed))
+			{
+				SCANcontroller.controller.map_flags = !SCANcontroller.controller.map_flags;
+			}
+
+			if (GUILayout.Button("Asteroids", SCANskins.SCAN_buttonFixed))
+			{
+				SCANcontroller.controller.map_asteroids = !SCANcontroller.controller.map_asteroids;
+			}
+
+			if (GUILayout.Button("Legend", SCANskins.SCAN_buttonFixed))
+			{
+				SCANcontroller.controller.legend = !SCANcontroller.controller.legend;
+			}
+
+			if (GUILayout.Button("Res_Overlay", SCANskins.SCAN_buttonFixed))
+			{
+				SCANcontroller.controller.map_ResourceOverlay = !SCANcontroller.controller.map_ResourceOverlay;
+				bigmap.resetMap();
+			}
+
+			stopS();
+		}
+
+		//Draw the actual map texture
+		private void mapDraw (int id)
+		{
 			Texture2D map = bigmap.getPartialMap();
 
 			GUILayout.Label("", GUILayout.Width(map.width), GUILayout.Height(map.height));
@@ -93,8 +225,82 @@ namespace SCANsat.SCAN_UI
 			}
 
 			GUI.DrawTexture(maprect, map);
+		}
 
-			stopS();
+		//Draw the altitude legend bar along the bottom
+		private void legendBar (int id)
+		{
+			SCANuiUtil.drawLegend(bigmap);
+		}
+
+		//Draw the drop down menus if any have been opened
+		private void dropDown(int id)
+		{
+			if (projection_drop_down)
+			{
+				ddRect = new Rect(30, 50, 100, 90);
+				GUI.Box(ddRect, "", SCANskins.SCAN_dropDownBox);
+				for (int i = 0; i < SCANmap.projectionNames.Length; ++i)
+				{
+					Rect r = new Rect(ddRect.x + 2, ddRect.y + (24 * i), ddRect.width - 4, 20);
+					if (GUI.Button(r, SCANmap.projectionNames[i], SCANskins.SCAN_dropDownButton))
+					{
+						bigmap.setProjection((SCANmap.MapProjection)i);
+						SCANcontroller.controller.projection = i;
+						overlay_static_dirty = true;
+						drop_down_open = false;
+					}
+				}
+			}
+
+			else if (mapType_drop_down)
+			{
+				ddRect = new Rect(30, 50, 100, 90);
+				GUI.Box(ddRect, "", SCANskins.SCAN_dropDownBox);
+				for (int i = 0; i < 3; i++)
+				{
+					Rect r = new Rect(ddRect.x + 2, ddRect.y + (24 * i), ddRect.width - 4, 20);
+					if (GUI.Button(r, "Map Type Goes Here", SCANskins.SCAN_dropDownButton))
+					{
+						bigmap.resetMap(i, 0);
+						drop_down_open = false;
+					}
+				}
+			}
+
+			else if (resources_drop_down)
+			{
+				ddRect = new Rect(30, 50, 100, SCANcontroller.ResourcesList.Count * 20);
+				GUI.Box(ddRect, "", SCANskins.SCAN_dropDownBox);
+				for (int i = 0; i < SCANcontroller.ResourcesList.Count; i++)
+				{
+					Rect r = new Rect(ddRect.x + 2, ddRect.y + (24 * i), ddRect.width - 4, 20);
+					if (GUI.Button(r, SCANcontroller.ResourcesList[i].name, SCANskins.SCAN_dropDownButton))
+					{
+						SCANcontroller.controller.gridSelection = i;
+						drop_down_open = false;
+					}
+				}
+			}
+
+			else if (planetoid_drop_down)
+			{
+				ddRect = new Rect(30, 50, 100, FlightGlobals.Bodies.Count * 20);
+				GUI.Box(ddRect, "", SCANskins.SCAN_dropDownBox);
+				for (int i = 0; i < FlightGlobals.Bodies.Count; i++)
+				{
+					Rect r = new Rect(ddRect.x + 2, ddRect.y + (24 * i), ddRect.width - 4, 20);
+					if (GUI.Button(r, FlightGlobals.Bodies[i].theName, SCANskins.SCAN_dropDownButton))
+					{
+						bigmap.resetMap();
+						drop_down_open = false;
+					}
+				}
+			}
+
+			else
+				drop_down_open = false;
+
 		}
 
 	}
