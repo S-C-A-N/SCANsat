4a1662cc91b9be5da2a42a7d9a95fd74da4f91d3
DMagic <david.grandy@gmail.com>
2014-10-16 15:45:35 -0400

    New classes; start work on loading palettes

---
+++ SCANsat/SCAN_KSPPP/Palettes/Palettes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace SCANsat.Platform.Palettes
+{
+	public class Palettes
+	{
+		internal Palette defaultPalette;
+		internal Palette[] availablePalettes;
+		internal Palette.Kind paletteType;
+		int paletteSize;
+
+		public Palettes (Palette[] p, Palette.Kind pK, int i)
+		{
+			availablePalettes = p;
+			paletteType = pK;
+			paletteSize = i;
+		}
+	}
+}
+
