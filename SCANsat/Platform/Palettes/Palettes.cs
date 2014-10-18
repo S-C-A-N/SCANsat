00950da3a4e9ee5a2632c0357af4fb4668cac4f0
DMagic <david.grandy@gmail.com>
2014-10-18 01:44:35 -0400

    generate swatch textures

--- SCANsat/Platform/Palettes/Palettes.cs
+++ SCANsat/Platform/Palettes/Palettes.cs
@@ -6,16 +6,48 @@ namespace SCANsat.Platform.Palettes
 {
 	public class Palettes
 	{
-		internal Palette defaultPalette;
 		internal Palette[] availablePalettes;
 		internal Palette.Kind paletteType;
-		int paletteSize;
+		internal Texture2D[] paletteSwatch;
+		private int paletteSize;
 
-		public Palettes (Palette[] p, Palette.Kind pK, int i)
+		internal Palettes (Palette[] p, Palette.Kind pK, int i)
 		{
 			availablePalettes = p;
 			paletteType = pK;
 			paletteSize = i;
+			generateSwatches();
+		}
+
+		private Texture2D[] generateSwatches()
+		{
+			if (paletteSwatch == null)
+			{
+				paletteSwatch = new Texture2D[availablePalettes.Length];
+				for (int i = 0; i < availablePalettes.Length; i++)
+				{
+					int k = 0;
+					int m = 120;
+					if (paletteSize == 11) m = 121;
+					else if (paletteSize == 9) m = 117;
+					else if (paletteSize == 7) m = 119;
+					Texture2D t = new Texture2D(m, 1);
+					Color[] pix = t.GetPixels();
+					int sW = m / paletteSize;
+					for (int j = 0; j < m; j++)
+					{
+						if (j % sW == 0)
+							k++;
+						pix[j] = availablePalettes[i].colors[k-1];
+					}
+					t.SetPixels(pix);
+					t.Apply();
+					paletteSwatch[i] = t;
+				}
+				return paletteSwatch;
+			}
+			else
+				return paletteSwatch;
 		}
 	}
 }
