65370e3e1e354e06e3e989ece68d7b3bf2e5239c
DMagic <david.grandy@gmail.com>
2014-10-24 15:15:13 -0400

    changes to drawlegend

--- SCANsat/SCAN_UI/SCANbigMap.cs
+++ SCANsat/SCAN_UI/SCANbigMap.cs
@@ -580,14 +580,14 @@ namespace SCANsat.SCAN_UI
 				stopE();
 				SCANuiUtil.mouseOverInfo(mlon, mlat, bigmap, data, v.mainBody, in_map);
 				if (bigmap.mapmode == 0 && SCANcontroller.controller.legend)
-					SCANuiUtil.drawLegend(data.MinHeight, data.MaxHeight, data);
+					SCANuiUtil.drawLegend(data);
 			}
 			else
 			{
 				growS();
 				SCANuiUtil.mouseOverInfo(mlon, mlat, bigmap, data, v.mainBody, in_map);
 				if (bigmap.mapmode == 0 && SCANcontroller.controller.legend)
-					SCANuiUtil.drawLegend(data.MinHeight, data.MaxHeight, data);
+					SCANuiUtil.drawLegend(data);
 				stopS();
 				stopE();
 			}
