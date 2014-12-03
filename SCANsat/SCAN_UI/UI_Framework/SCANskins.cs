4eba86faab4863849006acc7067d0d45155b581d
DMagic <david.grandy@gmail.com>
2014-12-02 22:45:02 -0500

    Generate fixed palette set

--- SCANsat/SCAN_UI/UI_Framework/SCANskins.cs
+++ SCANsat/SCAN_UI/UI_Framework/SCANskins.cs
@@ -111,6 +111,7 @@ namespace SCANsat.SCAN_UI
 			palette.DivPaletteSet = palette.generatePaletteSet(7, Palette.Kind.Diverging);
 			palette.QualPaletteSet = palette.generatePaletteSet(7, Palette.Kind.Qualitative);
 			palette.SeqPaletteSet = palette.generatePaletteSet(7, Palette.Kind.Sequential);
+			palette.FixedPaletteSet = palette.generatePaletteSet(0, Palette.Kind.Fixed);
 			palette.CurrentPalettes = palette.DivPaletteSet;
 			palette.CurrentPalette = PaletteLoader.defaultPalette;
 		}
