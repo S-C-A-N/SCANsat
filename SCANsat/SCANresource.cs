e7a05de3f7c4a62127e9ffc8d53540e80f6b0529
DMagic <david.grandy@gmail.com>
2015-01-07 17:30:00 -0500

    Move these out of SCANdata

---
+++ SCANsat/SCANresource.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using palette = SCANsat.SCAN_UI.SCANpalette;
+using UnityEngine;
+
+namespace SCANsat
+{
+	public enum SCANresource_Source
+	{
+		Kethane = 1,
+		Regolith = 2,
+	}
+
+	public class SCANresource
+	{
+		internal SCANresource(string n, string Body, Color full, Color empty, float min, float max, SCANresourceType t, SCANresource_Source s)
+		{
+			name = n;
+			body = Body;
+			fullColor = full;
+			emptyColor = empty;
+			minValue = min;
+			maxValue = max;
+			resourceType = t;
+			type = resourceType.Type;
+			source = s;
+		}
+
+		private string name;
+		private string body;
+		private Color fullColor, emptyColor;
+		private float minValue, maxValue;
+		private SCANtype type;
+		private SCANresourceType resourceType;
+		private SCANresource_Source source;
+
+		public string Name
+		{
+			get { return name; }
+		}
+
+		public string Body
+		{
+			get { return body; }
+		}
+
+		public Color FullColor
+		{
+			get { return fullColor; }
+		}
+
+		public Color EmptyColor
+		{
+			get { return emptyColor; }
+		}
+
+		public SCANtype Type
+		{
+			get { return type; }
+		}
+
+		public SCANresourceType ResourceType
+		{
+			get { return resourceType; }
+		}
+
+		public float MinValue
+		{
+			get { return minValue; }
+		}
+		
+		public float MaxValue
+		{
+			get { return maxValue; }
+		}
+
+		public SCANresource_Source Source
+		{
+			get { return source; }
+		}
+	}
+
+	public class SCANresourceType
+	{
+		private string name;
+		private SCANtype type;
+		private Color colorFull, colorEmpty;
+
+		internal SCANresourceType(string s, int i, string Full, string Empty)
+		{
+			name = s;
+			type = (SCANtype)i;
+			if ((type & SCANtype.Everything_SCAN) != SCANtype.Nothing)
+			{
+				Debug.LogWarning("[SCANsat] Attempt To Override Default SCANsat Sensors; Resetting Resource Scanner Type To 0");
+				type = SCANtype.Nothing;
+			}
+			try
+			{
+				colorFull = ConfigNode.ParseColor(Full);
+			}
+			catch (Exception e)
+			{
+				SCANUtil.SCANlog("Color Format Incorrect; Reverting To Default Full Resource Color: {0}", e);
+				colorFull = palette.cb_reddishPurple;
+			}
+			try
+			{
+				colorEmpty = ConfigNode.ParseColor(Empty);
+			}
+			catch (Exception e)
+			{
+				SCANUtil.SCANlog("Color Format Incorrect; Reverting To Default Empty Resource Color: {0}", e);
+				colorEmpty = palette.magenta;
+			}
+		}
+
+		public string Name
+		{
+			get { return name; }
+		}
+
+		public SCANtype Type
+		{
+			get { return type;}
+		}
+
+		public Color ColorFull
+		{
+			get { return colorFull; }
+		}
+
+		public Color ColorEmpty
+		{
+			get { return colorEmpty; }
+		}
+	}
+}
---
+++ SCANsat/SCANtype.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SCANsat
+{
+	public enum SCANtype : int
+	{
+		Nothing = 0, 		    // no data (MapTraq)
+		AltimetryLoRes = 1 << 0,  // low resolution altimetry (limited zoom)
+		AltimetryHiRes = 1 << 1,  // high resolution altimetry (unlimited zoom)
+		Altimetry = (1 << 2) - 1, 	        // both (setting) or either (testing) altimetry
+		SCANsat_1 = 1 << 2,		// Unused, reserved for future SCANsat scanner
+		Biome = 1 << 3,		    // biome data
+		Anomaly = 1 << 4,		    // anomalies (position of anomaly)
+		AnomalyDetail = 1 << 5,	// anomaly detail (name of anomaly, etc.)
+		Kethane = 1 << 6,         // Kethane - K-type - Kethane
+		Ore = 1 << 7,             // Ore - ORS & K-type - EPL & MKS
+		Kethane_3 = 1 << 8,       // Reserved - K-type
+		Kethane_4 = 1 << 9,       // Reserved - K-type
+		Uranium = 1 << 10,        // Uranium - ORS - KSPI
+		Thorium = 1 << 11,        // Thorium - ORS - KSPI
+		Alumina = 1 << 12,        // Alumina - ORS - KSPI
+		Water = 1 << 13,          // Water - ORS - KSPI
+		Aquifer = 1 << 14,        // Aquifer - ORS & K-type - MKS
+		Minerals = 1 << 15,       // Minerals - ORS & K-type - MKS
+		Substrate = 1 << 16,      // Substrate - ORS & K-type - MKS
+		KEEZO = 1 << 17,          // KEEZO - ORS - Kass Effect
+		Karbonite = 1 << 18,    // Karbonite - ORS
+		ORS_10 = 1 << 19,         // Reserved - ORS
+
+		Everything_SCAN = (1 << 6) - 1,	// All default SCANsat scanners
+		Everything = Int32.MaxValue      // All scanner types
+	}
+}
