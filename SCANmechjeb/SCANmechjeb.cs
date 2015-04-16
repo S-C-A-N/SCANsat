261971ab7dc3b7030ccf573ac3cacd4371c3f853
DMagic <david.grandy@gmail.com>
2015-04-16 00:34:25 -0400

    Add MechJeb interface project

---
+++ SCANmechjeb/SCANmechjeb.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SCANsat;
+using SCANsat.SCAN_Platform;
+using MuMech;
+
+namespace SCANmechjeb
+{
+	class SCANmechjeb : SCAN_MBE
+	{
+		MechJebWaypoint w;
+	}
+}
