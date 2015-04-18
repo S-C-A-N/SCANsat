a16e8b53964c9f9be1f069d4fc1c5adbc2be1abf
DMagic <david.grandy@gmail.com>
2015-04-18 12:56:56 -0400

    Unused code

--- SCANmechjeb/SCANmechjeb.cs
+++ SCANmechjeb/SCANmechjeb.cs
@@ -185,27 +185,10 @@ namespace SCANmechjeb
 
 			if (!MapView.MapIsEnabled)
 				return;
-			//if (MapViewTarget(MapView.MapCamera.target)!= SCANcontroller.controller.MechJebTargetBody)
-			//	return;
 			if (!v.isActiveVessel || v.GetMasterMechJeb() != core)
 				return;
 
 			GLUtils.DrawMapViewGroundMarker(SCANcontroller.controller.MechJebTargetBody, coords.y, coords.x, palette.mechjebYellow);
-		}
-
-		private CelestialBody MapViewTarget(MapObject target)
-		{
-			switch(target.type)
-			{
-				case MapObject.MapObjectType.VESSEL:
-					return target.vessel.mainBody;
-				case MapObject.MapObjectType.CELESTIALBODY:
-					return target.celestialBody;
-				case MapObject.MapObjectType.MANEUVERNODE:
-					return target.maneuverNode.patch.referenceBody;
-			}
-
-			return null;
 		}
 	}
 }
