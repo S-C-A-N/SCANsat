5c9c8e9cb58e1b9a11cb317b046c3f4bd96a2a82
DMagic <david.grandy@gmail.com>
2015-04-29 17:57:08 -0400

    New resource part module

---
+++ SCANsat/SCANresourceScanner.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SCANsat.SCAN_Data;
+
+
+namespace SCANsat
+{
+	public class ModuleSCANresourceScanner : SCANsat, IAnimatedModule
+	{
+		[KSPField]
+		public bool scanner = false;
+
+		private ModuleOrbitalSurveyor mSurvey;
+		private ModuleResourceScanner mScanner;
+		private bool bodyScanned = false;
+		private bool bodyCoverage = false;
+
+		public override void OnStart(PartModule.StartState state)
+		{
+			base.OnStart(state);
+
+			mSurvey = findSurvey();
+			mScanner = findScanner();
+		}
+
+		private ModuleResourceScanner findScanner()
+		{
+			ModuleResourceScanner r = vessel.FindPartModulesImplementing<ModuleResourceScanner>().FirstOrDefault();
+			return r;
+		}
+
+		private ModuleOrbitalSurveyor findSurvey()
+		{
+			ModuleOrbitalSurveyor s = vessel.FindPartModulesImplementing<ModuleOrbitalSurveyor>().FirstOrDefault();
+			return s;
+		}
+
+		private void updateEvents()
+		{
+			base.Events["startScan"].active = !scanning && isEnabled;
+			base.Events["stopScan"].active = scanning && isEnabled;
+		}
+
+		private void Update()
+		{
+			if (!HighLogic.LoadedSceneIsFlight)
+				return;
+
+			if (SCANcontroller.controller == null)
+				return;
+
+			if (!SCANcontroller.controller.easyModeScanning)
+				updateEvents();
+		}
+
+		public void DisableModule()
+		{
+			this.isEnabled = false;
+			unregisterScanner();
+		}
+
+		public void EnableModule()
+		{
+			this.isEnabled = true;
+		}
+
+		public bool IsSituationValid()
+		{
+			return true;
+		}
+
+		public bool ModuleIsActive()
+		{
+			return isEnabled;
+		}
+	}
+}
