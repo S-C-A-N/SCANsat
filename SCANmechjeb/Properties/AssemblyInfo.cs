1b9515ef6627e012b222bbb9e24298d4793a568e
DMagic <david.grandy@gmail.com>
2015-05-30 15:59:01 -0400

    Versions

--- SCANmechjeb/Properties/AssemblyInfo.cs
+++ SCANmechjeb/Properties/AssemblyInfo.cs
@@ -32,10 +32,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion ("v12")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyInformationalVersion ("v12.1")]
 
-[assembly: KSPAssembly ("SCANmechjeb", 0, 3)]
+[assembly: KSPAssembly ("SCANmechjeb", 0, 4)]
 [assembly: KSPAssemblyDependency ("SCANsat", 1, 2)]
 [assembly: KSPAssemblyDependency("MechJeb2", 2, 5)]
