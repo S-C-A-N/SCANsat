66486fbf25f79ac39c08eb5f1fb0656e44a4e6eb
DMagic <david.grandy@gmail.com>
2015-06-25 16:20:51 -0400

    Unused code

--- SCANsat/SCAN_UI/SCANmainMap.cs
+++ SCANsat/SCAN_UI/SCANmainMap.cs
@@ -72,7 +72,6 @@ namespace SCANsat.SCAN_UI
 				SCANcontroller.controller.addToBodyData(v.mainBody, data);
 			}
 			sensors = SCANcontroller.controller.activeSensorsOnVessel(v.id);
-			//data.updateImages(sensors);
 		}
 
 		protected override void DrawWindow(int id)
@@ -128,14 +127,12 @@ namespace SCANsat.SCAN_UI
 		//Draw the map texture
 		private void mainMap(int id)
 		{
-			//GUILayout.Label(data.Map);
-			//mapRect = GUILayoutUtility.GetLastRect();
 			mapRect = new Rect(10, 20, 360, 180);
 			GUI.DrawTexture(mapRect, data.drawPartialMap(sensors));
 
 			if (data.Building)
 			{
-				GUI.Label(new Rect(mapRect.x + 90, mapRect.y + 60, 180, 60), "");
+				GUI.Label(new Rect(mapRect.x + 90, mapRect.y + 60, 180, 60), "Building Database...");
 			}
 
 			GUILayout.Space(184);
