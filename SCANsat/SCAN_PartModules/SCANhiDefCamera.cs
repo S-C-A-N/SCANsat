4bb4065e770ad73c7978d938bc9912778f0c9e85
DMagic <david.grandy@gmail.com>
2015-06-25 16:23:19 -0400

    IAnimatedModule for this

--- SCANsat/SCAN_PartModules/SCANhiDefCamera.cs
+++ SCANsat/SCAN_PartModules/SCANhiDefCamera.cs
@@ -2,20 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using SCANsat.SCAN_UI;
+using UnityEngine;
 
 namespace SCANsat.SCAN_PartModules
 {
-	public class SCANhiDefCamera : PartModule
+	public class SCANhiDefCamera : PartModule, IAnimatedModule
 	{
 		[KSPField]
+		public float minZoom = 2;
+		[KSPField]
+		public float maxZoom = 1000;
+		[KSPField]
 		public bool hiDetailOnly;
+
+		private List<ModuleHighDefCamera> stockCameras;
 
 		public override void OnStart(PartModule.StartState state)
 		{
 			base.OnStart(state);
 
+			stockCameras = findCameras();
+
+			minZoom = clampValue(minZoom, 1, 10);
+			maxZoom = clampValue(maxZoom, 10, 1000);
+
 			Events["toggleSCANHiDef"].guiName = "Toggle Map";
 			Events["resetCenter"].guiName = "Reset Map Center";
+		}
+
+		private List<ModuleHighDefCamera> findCameras()
+		{
+			return part.FindModulesImplementing<ModuleHighDefCamera>().ToList();
+		}
+
+		private float clampValue (float value, float min, float max)
+		{
+			return Mathf.Clamp(value, min, max);
 		}
 
 		[KSPEvent(guiActive = true, active = true)]
@@ -28,7 +50,7 @@ namespace SCANsat.SCAN_PartModules
 
 			if (!SCANcontroller.controller.hiDefMap.Visible)
 			{
-				SCANcontroller.controller.hiDefMap.setMapCenter(SCANUtil.fixLatShift(vessel.latitude), SCANUtil.fixLonShift(vessel.longitude), hiDetailOnly || SCANcontroller.controller.hiDetailZoomMap);
+				SCANcontroller.controller.hiDefMap.setMapCenter(SCANUtil.fixLatShift(vessel.latitude), SCANUtil.fixLonShift(vessel.longitude), hiDetailOnly || SCANcontroller.controller.hiDetailZoomMap, null, this);
 			}
 			else
 				SCANcontroller.controller.hiDefMap.Visible = false;
@@ -42,7 +64,54 @@ namespace SCANsat.SCAN_PartModules
 			if (SCANcontroller.controller.hiDefMap == null)
 				return;
 
-			SCANcontroller.controller.hiDefMap.setMapCenter(SCANUtil.fixLatShift(vessel.latitude), SCANUtil.fixLonShift(vessel.longitude), hiDetailOnly || SCANcontroller.controller.hiDetailZoomMap);
+			SCANcontroller.controller.hiDefMap.setMapCenter(SCANUtil.fixLatShift(vessel.latitude), SCANUtil.fixLonShift(vessel.longitude), hiDetailOnly || SCANcontroller.controller.hiDetailZoomMap, null, this);
 		}
+
+		private void enableConnectedModules()
+		{
+			if (stockCameras != null)
+			{
+				foreach (ModuleHighDefCamera m in stockCameras)
+				{
+					m.EnableModule();
+				}
+			}
+		}
+
+		private void disableConnectedModules()
+		{
+			if (stockCameras != null)
+			{
+				foreach (ModuleHighDefCamera m in stockCameras)
+				{
+					m.DisableModule();
+				}
+			}
+		}
+
+		public void EnableModule()
+		{
+			isEnabled = true;
+			if (SCANcontroller.controller != null && SCANcontroller.controller.disableStockResource)
+				disableConnectedModules();
+		}
+
+		public void DisableModule()
+		{
+			isEnabled = false;
+			if (SCANcontroller.controller != null && SCANcontroller.controller.disableStockResource)
+				disableConnectedModules();
+		}
+
+		public bool ModuleIsActive()
+		{
+			return isEnabled;
+		}
+
+		public bool IsSituationValid()
+		{
+			return true;
+		}
+
 	}
 }
