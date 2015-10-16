073771f1083ac7973d493afd03b060af18bb8892
DMagic <david.grandy@gmail.com>
2015-10-16 13:39:21 -0400

    New language pack classes

---
+++ SCANsat/SCAN_UI/UI_Framework/SCANlanguagePack.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SCANsat.SCAN_Platform;
+
+namespace SCANsat.SCAN_UI.UI_Framework
+{
+	public class SCANlanguagePack : SCAN_ConfigNodeStorage
+	{
+		[Persistent]
+		public bool activePack = true;
+		[Persistent]
+		public string language = "English (USA)";
+
+		//Settings Window Help Tooltips
+		[Persistent]
+		public string settingsHelpAnomalies = "";
+		[Persistent]
+		public string settingsHelpBackground = "";
+		[Persistent]
+		public string settingsHelpTimeWarp = "";
+		[Persistent]
+		public string settingsHelpGroundTracks = "";
+		[Persistent]
+		public string settingsHelpGroundTracksActive = "";
+		[Persistent]
+		public string settingsHelpOverlayTooltips = "";
+		[Persistent]
+		public string settingsHelpWindowTooltips = "";
+		[Persistent]
+		public string settingsHelpStockToolbar = "";
+		[Persistent]
+		public string settingsHelpMechJeb = "";
+		[Persistent]
+		public string settingsHelpResetWindows = "";
+		[Persistent]
+		public string settingsHelpResetPlanetData = "";
+		[Persistent]
+		public string settingsHelpResetAllData = "";
+		[Persistent]
+		public string settingsHelpVesselsSensorsPasses = "";
+
+		//Resource Settings Window Help Tooltips
+		[Persistent]
+		public string resourceSettingsHelpOverlayWindow = "";
+		[Persistent]
+		public string resourceSettingsHelpBiomeLock = "";
+		[Persistent]
+		public string resourceSettingsHelpInstant = "";
+		[Persistent]
+		public string resourceSettingsHelpNarrowBand = "";
+		[Persistent]
+		public string resourceSettingsHelpDisableStock = "";
+		[Persistent]
+		public string resourceSettingsHelpResetSCANsatResource = "";
+		[Persistent]
+		public string resourceSettingsHelpResetStockResource = "";
+		[Persistent]
+		public string resourceSettingsHelpOverlayInterpolation = "";
+		[Persistent]
+		public string resourceSettingsHelpOverlayHeight = "";
+		[Persistent]
+		public string resourceSettingsHelpOverlayBiomeHeight = "";
+		[Persistent]
+		public string resourceSettingsHelpOverlayTransparency = "";
+
+	}
+}
---
+++ SCANsat/SCAN_UI/UI_Framework/SCANlocalization.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SCANsat.SCAN_Platform;
+
+namespace SCANsat.SCAN_UI.UI_Framework
+{
+	public class SCANlocalization : SCAN_ConfigNodeStorage
+	{
+		private SCANlanguagePack activePack;
+
+		[Persistent]
+		private List<SCANlanguagePack> languagePacks = new List<SCANlanguagePack>();
+
+		public SCANlocalization(string path, string node)
+		{
+			FilePath = path;
+			TopNodeName = node;
+
+			Load();
+		}
+
+		public override void OnDecodeFromConfigNode()
+		{
+			activePack = languagePacks.FirstOrDefault(l => l.activePack);
+
+			if (activePack == null)
+				activePack = new SCANlanguagePack();
+		}
+
+		public SCANlanguagePack ActivePack
+		{
+			get { return activePack; }
+		}
+	}
+}
