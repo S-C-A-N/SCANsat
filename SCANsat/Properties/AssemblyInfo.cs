5b4d6dffb0549e8c206d6de99e43c551dae3b3b2
DMagic <david.grandy@gmail.com>
2016-10-12 13:27:38 -0400

    version

--- SCANsat/Properties/AssemblyInfo.cs
+++ SCANsat/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.6.0.8")]
-[assembly: AssemblyFileVersion ("1.6.0.8")]
-[assembly: AssemblyInformationalVersion ("v16.8")]
+[assembly: AssemblyVersion ("1.6.0.9")]
+[assembly: AssemblyFileVersion ("1.6.0.9")]
+[assembly: AssemblyInformationalVersion ("v16.9")]
 
 [assembly: KSPAssembly ("SCANsat", 1, 6)]
 
