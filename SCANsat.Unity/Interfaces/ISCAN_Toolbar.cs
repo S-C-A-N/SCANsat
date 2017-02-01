b552c85f6d0c6bec4d6e4249fdee69500e323a68
DMagic <david.grandy@gmail.com>
2017-02-01 12:59:03 -0500

    Toolbar menu implementation

---
+++ SCANsat.Unity/Interfaces/ISCAN_Toolbar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace SCANsat.Unity.Interfaces
+{
+	public interface ISCAN_Toolbar
+	{
+		Canvas TooltipCanvas { get; }
+
+		bool TooltipsOn { get; }
+
+		bool MainMap { get; set; }
+
+		bool BigMap { get; set; }
+
+		bool ZoomMap { get; set; }
+
+		bool Overlay { get; set; }
+
+		bool Instruments { get; set; }
+
+		bool Settings { get; set; }
+
+		bool InMenu { get; set; }
+
+		float Scale { get; }
+	}
+}
