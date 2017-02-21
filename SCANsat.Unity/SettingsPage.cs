799ecd74a680fdfa09e1f01cef919d14cdc16d00
DMagic <david.grandy@gmail.com>
2017-02-21 11:37:24 -0500

    Method for handling clicking on empty spaces

--- SCANsat.Unity/SettingsPage.cs
+++ SCANsat.Unity/SettingsPage.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace SCANsat.Unity
 {
 	public class SettingsPage : MonoBehaviour
 	{
-
+		public virtual void OnPointerDown(PointerEventData eventData) { }
 	}
 }
