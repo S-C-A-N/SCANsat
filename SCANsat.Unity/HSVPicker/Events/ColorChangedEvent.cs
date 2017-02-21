aa4784e9a78405ee8186d58edff9dc07e55c7ad2
DMagic <david.grandy@gmail.com>
2017-02-21 11:32:04 -0500

    New color picker implementation

---
+++ SCANsat.Unity/HSVPicker/Events/ColorChangedEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+namespace SCANsat.Unity.HSVPicker.Events
+{
+	[Serializable]
+	public class ColorChangedEvent : UnityEvent<Color>
+	{
+		
+	}
+}
---
+++ SCANsat.Unity/HSVPicker/Events/HSVChangedEvent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace SCANsat.Unity.HSVPicker.Events
+{
+	public class HSVChangedEvent : UnityEvent<float, float, float>
+	{
+
+	}
+}
