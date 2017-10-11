5a3be74792d6a1d2aada22dc7a6f9a7597b8a093
DMagic <david.grandy@gmail.com>
2017-10-11 12:15:11 -0400

    License info

--- SCANsat/SCAN_Palettes/SCANPalette.cs
+++ SCANsat/SCAN_Palettes/SCANPalette.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿#region license
+/* 
+ * [Scientific Committee on Advanced Navigation]
+ * 			S.C.A.N. Satellite
+ *
+ * SCANPalette - Class to hold color palette info
+ * 
+ * Copyright (c)2014 David Grandy <david.grandy@gmail.com>;
+ * Copyright (c)2014 technogeeky <technogeeky@gmail.com>;
+ * Copyright (c)2014 (Your Name Here) <your email here>; see LICENSE.txt for licensing details.
+ */
+#endregion
+
+using System;
 using UnityEngine;
 using SCANsat.SCAN_Platform;
 
