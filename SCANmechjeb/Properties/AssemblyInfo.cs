bb7480b4ee45ca1b83c71366a27934729759727b
DMagic1 <david.grandy@gmail.com>
2018-12-05 11:11:41 -0500

    Version

--- SCANmechjeb/Properties/AssemblyInfo.cs
+++ SCANmechjeb/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.8.0")]
-[assembly: AssemblyFileVersion("1.8.8.0")]
-[assembly: AssemblyInformationalVersion ("v18.8")]
+[assembly: AssemblyVersion("1.8.9.0")]
+[assembly: AssemblyFileVersion("1.8.9.0")]
+[assembly: AssemblyInformationalVersion ("v18.9")]
 
 [assembly: KSPAssembly ("SCANmechjeb", 0, 5)]
 [assembly: KSPAssemblyDependency ("SCANsat", 1, 8)]
