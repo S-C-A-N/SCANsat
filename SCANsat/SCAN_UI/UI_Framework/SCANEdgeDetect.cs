a97e67f3658d5d3e2af418aa4826da569eb83549
DMagic1 <david.grandy@gmail.com>
2019-10-28 20:32:44 -0700

    New component to handle BTDT image processing

---
+++ SCANsat/SCAN_UI/UI_Framework/SCANEdgeDetect.cs
@@ -0,0 +1,62 @@
+﻿
+using UnityEngine;
+using SCANsat.SCAN_Unity;
+using palette = SCANsat.SCAN_UI.UI_Framework.SCANcolorUtil;
+
+namespace SCANsat.SCAN_UI.UI_Framework
+{
+    public class SCANEdgeDetect : MonoBehaviour
+    {
+        private float _sensitivityDepth = 0.8f;
+        private float _sensitivityNormals = 0.6f;
+        private float _sampleDist = 0.8f;
+
+        private Material _edgeDetectMaterial = null;
+
+        private void Start()
+        {
+            SetMaterial();
+        }
+
+        private void SetMaterial()
+        {
+            _edgeDetectMaterial = new Material(SCAN_UI_Loader.EdgeDetectShader);
+
+            Texture2D t = new Texture2D(256, 1, TextureFormat.RGB24, false);
+
+            // ramp texture to render everything in dark shades of Amber,
+            // except originally dark lines, which become bright Amber
+            for (int i = 0; i < 256; ++i)
+                t.SetPixel(i, 0, palette.lerp(palette.black, palette.xkcd_Amber, i / 1024f));
+            for (int i = 0; i < 10; ++i)
+                t.SetPixel(i, 0, palette.xkcd_Amber);
+
+            t.Apply();
+
+            _edgeDetectMaterial.SetTexture("_RampTex", t);
+            Vector2 sensitivity = new Vector2(_sensitivityDepth, _sensitivityNormals);
+            _edgeDetectMaterial.SetVector("_Sensitivity", new Vector4(sensitivity.x, sensitivity.y, 1.0f, sensitivity.y));
+            _edgeDetectMaterial.SetFloat("_SampleDistance", _sampleDist);
+        }
+
+        private void OnEnable()
+        {
+            SetCameraFlag();
+        }
+
+        private void SetCameraFlag()
+        {
+            GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
+        }
+
+        //Camera RenderTexture will be applied here
+        [ImageEffectOpaque]
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (_edgeDetectMaterial == null)
+                SetMaterial();
+
+            Graphics.Blit(source, destination, _edgeDetectMaterial);
+        }
+    }
+}
