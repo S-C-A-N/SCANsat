ab2d110582b233b7ca6cb2c63b78d9603b672c26
DMagic1 <david.grandy@gmail.com>
2020-01-27 19:15:48 -0800

    Version

--- SCANmechjeb/Properties/AssemblyInfo.cs
+++ SCANmechjeb/Properties/AssemblyInfo.cs
@@ -32,10 +32,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.14.0")]
-[assembly: AssemblyFileVersion("1.8.14.0")]
-[assembly: AssemblyInformationalVersion ("v18.14")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyInformationalVersion ("v19.0")]
 
 [assembly: KSPAssembly ("SCANmechjeb", 0, 5)]
-[assembly: KSPAssemblyDependency ("SCANsat", 1, 8)]
+[assembly: KSPAssemblyDependency ("SCANsat", 1, 9)]
 [assembly: KSPAssemblyDependency("MechJeb2", 2, 5)]
--- SCANsat/Properties/AssemblyInfo.cs
+++ SCANsat/Properties/AssemblyInfo.cs
@@ -14,10 +14,10 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.8.14.0")]
-[assembly: AssemblyFileVersion ("1.8.14.0")]
-[assembly: AssemblyInformationalVersion ("v18.14")]
+[assembly: AssemblyVersion ("1.9.0.0")]
+[assembly: AssemblyFileVersion ("1.9.0.0")]
+[assembly: AssemblyInformationalVersion ("v19.0")]
 
-[assembly: KSPAssembly ("SCANsat", 1, 8)]
+[assembly: KSPAssembly ("SCANsat", 1, 9)]
 
 
